2d1399ab52906d347dfd936284433fc26a3d78ae
doseong <doseong0213@naver.com>
2025-08-30 22:26:27 +0900

    ✨ feat: list 삭제 기능 추가

--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -1,9 +1,12 @@
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, onClick }) {
   return (
     <div>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => onClick(todo.id)}>삭제</button>
+          </li>
         ))}
       </ul>
     </div>
--- my-app/src/pages/MainPage.js
+++ my-app/src/pages/MainPage.js
@@ -9,11 +9,14 @@ export default function MainPage() {
     setTodos((prev) => [...prev, text]);
   };
 
+  const handleDelete = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
   return (
     <div className="App">
       <h1 className="title">to do list</h1>
       <Input onAddTodos={handleAddUser} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onClick={handleDelete} />
     </div>
   );
 }

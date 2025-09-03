7c1bb4e5740fe7f237063c745ffa7280507c09f3
doseong <doseong0213@naver.com>
2025-09-03 13:17:24 +0900

    ✨ feat: edit 기능 추가

--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -1,11 +1,39 @@
-export default function TodoList({ todos, onClick }) {
+export default function TodoList({
+  todos,
+  onClick,
+  onHandleEdit,
+  editId,
+  editText,
+  onEditSave,
+  setEditText,
+}) {
   return (
     <div>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            {todo.text}
-            <button onClick={() => onClick(todo.id)}>삭제</button>
+            {editId === todo.id ? (
+              <>
+                <input
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      onEditSave();
+                    }
+                  }}
+                />
+                <button onClick={onEditSave}>저장</button>
+              </>
+            ) : (
+              <>
+                {todo.text}
+                <button onClick={() => onHandleEdit(todo.id)}>수정</button>
+                <button className="btn-delete" onClick={() => onClick(todo.id)}>
+                  삭제
+                </button>
+              </>
+            )}
           </li>
         ))}
       </ul>
--- my-app/src/pages/MainPage.js
+++ my-app/src/pages/MainPage.js
@@ -4,6 +4,8 @@ import { useState } from 'react';
 
 export default function MainPage() {
   const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(null);
+  const [editText, setEditText] = useState('');
 
   const handleAddUser = (text) => {
     setTodos((prev) => [...prev, text]);
@@ -12,11 +14,36 @@ export default function MainPage() {
   const handleDelete = (id) => {
     setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
+
+  const handleEdit = (editId) => {
+    const selectedTodo = todos.find((todo) => todo.id === editId);
+    setEditId(editId);
+    setEditText(selectedTodo.text);
+  };
+
+  const editSave = () => {
+    const newTodos = todos.map((todo) =>
+      todo.id === editId ? { ...todo, text: editText } : todo
+    );
+
+    setTodos(newTodos);
+    setEditId(null);
+    setEditText('');
+  };
+
   return (
     <div className="App">
       <h1 className="title">to do list</h1>
       <Input onAddTodos={handleAddUser} />
-      <TodoList todos={todos} onClick={handleDelete} />
+      <TodoList
+        todos={todos}
+        onClick={handleDelete}
+        onHandleEdit={handleEdit}
+        editId={editId}
+        editText={editText}
+        onEditSave={editSave}
+        setEditText={setEditText}
+      />
     </div>
   );
 }
